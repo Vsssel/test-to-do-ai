11edcf9851fc4dac2dda9c2180157e777901e812
Артыкбай Асел Айтбайкызы <assel.artykbay@ffin.life>
2025-11-14 16:03:45 +0500

    migrations init

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+    schema: "./packages/db/schema.ts",
+    out: "./packages/db/migrations",
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: process.env.DATABASE_URL as string
+    },
+    verbose: true,
+    strict: true
+})
